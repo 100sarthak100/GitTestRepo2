f830987626f6cd5a318f807b6b175e3b7945ea73
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-03-03 22:28:20 +0530

    Added a display function and a decorator function

--- TestFile_1.py
+++ TestFile_1.py
@@ -1,3 +1,19 @@
 list1 = [1, 2, 3, 4, 5]
 print(list(map(lambda x: (x) ** 0.5, list1)))
 
+
+def decorator_function(orig_func):
+    def wrapper_function(*args, **kwargs):
+        print(f"Wrapper executed this before {orig_func.__name__}")
+        return orig_func(*args, **kwargs)
+
+    return wrapper_function
+
+
+@decorator_function
+def display():
+    print("Display function ran")
+
+
+display()
+
